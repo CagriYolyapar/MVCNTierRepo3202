ca33cbbddc46c6ee6bb5895ae1957daed02e6504
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-01-07 16:32:59 +0300

    Add project files.

---
+++ Project.ENTITIES/Models/Article.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class Article:BaseEntity
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int? AppUserID { get; set; }
+        public int? CategoryID { get; set; }
+        public int? EditorID { get; set; }
+
+
+        //Relational Properties
+
+        public virtual AppUser AppUser { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual Editor Editor { get; set; }
+        public virtual List<ArticleTag> ArticleTags { get; set; }
+
+
+
+
+    }
+}
