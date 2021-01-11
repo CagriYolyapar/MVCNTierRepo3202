b1693c4f69cdcf73d7d67a5079be1dc857dc54a5
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-01-11 17:23:39 +0300

    Front end harici tamamlandı

---
+++ Project.BLL/DesignPatterns/RepositoryPattern/ConcRep/ArticleRepository.cs
@@ -0,0 +1,19 @@
+﻿using Project.BLL.DesignPatterns.RepositoryPattern.BaseRep;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DesignPatterns.RepositoryPattern.ConcRep
+{
+    public class ArticleRepository:BaseRepository<Article>
+    {
+
+        public override List<Article> GetAll()
+        {
+           return _db.Articles.Where(x => x.Category.Status != ENTITIES.Enums.DataStatus.Deleted).ToList();
+        }
+    }
+}
---
+++ Project.DAL/Context/MyContext.cs
@@ -0,0 +1,39 @@
+﻿using Project.ENTITIES.Models;
+using Project.MAP.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("myConnection")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AppUserMap());
+            modelBuilder.Configurations.Add(new EditorMap());
+            modelBuilder.Configurations.Add(new ProfileMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+            modelBuilder.Configurations.Add(new TagMap());
+            modelBuilder.Configurations.Add(new ArticleMap());
+            modelBuilder.Configurations.Add(new ArticleTagMap());
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<ArticleTag> ArticleTags { get; set; }
+        public DbSet<Editor> Editors { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+    }
+}
